0a47a39989f464aa5392fb76da029253e781536c
deepikaakshaya <deepikavijayakumar1998@gmail.com>
2021-08-18 17:58:57 +0530

    sve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice';
+  
+  title = 'Welcome to learn binding';
+
+  IcecreamAvailabilityStatus = "Not available"
+  serverName ='test';
+  cssStringVar: string= " ";
+
+  constructor() { }
+  
+  onClickEventStatus()
+  {
+    this.IcecreamAvailabilityStatus='Ice cream is available';
+  }
+
+  onUpdateServerName(event: Event) 
+  {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(this.serverName);
+  }
+  fruits = ['Mango', 'Orange', 'Apple'];
+
+
+
+  addFruit(item: string) {
+    this.fruits.push(item);
+  }  
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ChildComponent } from './child/child.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+  FormsModule,
+  NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
